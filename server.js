dc555606080e83e430fb1566646cf4a8baca607e
Khushwant <Technological.mess@gmail.com>
2022-11-28 19:17:51 -0500

    changed login payload

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ app.post("/api/user/login", (req, res) => {
     .checkUser(req.body)
     .then((user) => {
       let payload = {
-        _id: jwt_payload._id,
-        userName: jwt_payload.userName,
+        _id: user._id,
+        userName: user.userName,
       };
 
       let token = jwt.sign(payload, jwtOptions.secretOrKey);
